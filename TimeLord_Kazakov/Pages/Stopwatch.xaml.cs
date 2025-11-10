28df6cf6d4b3d6440317f644666422d9435a2152
qwelord <secondofall22@rambler.ru>
2025-11-10 15:37:58 +0500

    Выполненная программа на оценку 5. Казаков

--- TimeLord_Kazakov/Pages/Stopwatch.xaml.cs
+++ TimeLord_Kazakov/Pages/Stopwatch.xaml.cs
@@ -23,7 +23,9 @@ namespace TimeLord_Kazakov.Pages
     {
         public DispatcherTimer dispatcherTimer = new DispatcherTimer();
         public float full_second = 0;
+        private float lastIntervalTime = 0;
         public bool start_stopwatch = false;
+
         public Stopwatch()
         {
             InitializeComponent();
@@ -54,17 +56,96 @@ namespace TimeLord_Kazakov.Pages
 
         private void StartStopwatch(object sender, RoutedEventArgs e)
         {
+            
+
             if (start_stopwatch == false)
             {
+                dispatcherTimer.Tick -= TimerSecond;
+                dispatcherTimer.Tick -= BackStartStopwatchTick;
+
+                dispatcherTimer.Tick += TimerSecond;
+
                 full_second = 0;
                 dispatcherTimer.Start();
                 start_stopwatch = true;
                 start.Content = "Стоп";
+
+                lastIntervalTime = 0;
+                intervalsList.Items.Clear();
             }
             else {
                 dispatcherTimer.Stop();
                 start_stopwatch = false;
                 start.Content = "Начать"; }
+        }
+
+        private void AddInterval_Click(object sender, RoutedEventArgs e)
+        {
+            if (start_stopwatch)
+            {
+                float intervalDiff = full_second - lastIntervalTime;
+                lastIntervalTime = full_second;
+
+                float hours = (int)(intervalDiff / 60 / 60);
+                float minuts = (int)(intervalDiff / 60) - (hours * 60);
+                float seconds = intervalDiff - (hours * 60 * 60) - (minuts * 60);
+
+                string s_seconds = seconds.ToString();
+                if (seconds < 10) s_seconds = "0" + seconds;
+
+                string s_minuts = minuts.ToString();
+                if (minuts < 10) s_minuts = "0" + minuts;
+
+                string s_hours = hours.ToString();
+                if (hours < 10) s_hours = "0" + hours;
+
+                intervalsList.Items.Add($"{s_hours}:{s_minuts}:{s_seconds}");
+            }
+        }
+
+        private void BackStartStopwatchTick(object sesnder, EventArgs e)
+        {
+            if (full_second > 0)
+            {
+                full_second--;
+
+                float hours = (int)(full_second / 60 / 60);
+                float minuts = (int)(full_second / 60) - (hours * 60);
+                float seconds = full_second - (hours * 60 * 60) - (minuts * 60);
+
+                string s_seconds = seconds.ToString();
+                if (seconds < 10) s_seconds = "0" + seconds;
+
+                string s_minuts = minuts.ToString();
+                if (minuts < 10) s_minuts = "0" + minuts;
+
+                string s_hours = hours.ToString();
+                if (hours < 10) s_hours = "0" + hours;
+
+                time.Content = s_hours + ":" + s_minuts + ":" + s_seconds;
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                start_stopwatch = false;
+                start.Content = "Начать";
+            }
+        }
+
+        private void BackStartStopwatch(object sender, RoutedEventArgs e)
+        {
+            if (full_second > 0)
+            {
+                dispatcherTimer.Tick -= TimerSecond;
+
+                dispatcherTimer.Tick += BackStartStopwatchTick;
+
+                dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+                dispatcherTimer.Start();
+
+                start_stopwatch = true;
+                start.Content = "Стоп";
+            }
         }
     }
 }
